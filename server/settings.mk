8684a96a10ce12d1aa0982dccdad48da9c4e9e53
JoseMiguelPazPortilla <jpazportilla@frba.utn.edu.ar>
2026-04-02 10:47:37 -0300

    subo template cliente servidor

--- server/settings.mk
+++ server/settings.mk
@@ -8,6 +8,3 @@ STATIC_LIBPATHS=
 # Compiler flags
 CDEBUG=-g -Wall -DDEBUG -fdiagnostics-color=always
 CRELEASE=-O3 -Wall -DNDEBUG
-
-# Source files (*.c) to be excluded from tests compilation
-TEST_EXCLUDE=src/main.c
